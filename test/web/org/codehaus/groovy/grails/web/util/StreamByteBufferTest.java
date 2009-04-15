grails/grails

commit 4d7965c8c9f0cc70c74398a829fccd4fdfe81852

fix for GRAILS-4421,GRAILS-4408 and GRAILS-4422: Support for global caching of queries, ability to retain generated GSP source and pagination parameters to executeQuery

grails/src/web/org/codehaus/groovy/grails/web/pages/GroovyPageParser.java
@@ -16,6 +16,8 @@
 package org.codehaus.groovy.grails.web.pages;
 
 import grails.util.Environment;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -23,6 +25,8 @@ import org.codehaus.groovy.grails.commons.*;
 import org.codehaus.groovy.grails.web.taglib.GrailsTagRegistry;
 import org.codehaus.groovy.grails.web.taglib.GroovySyntaxTag;
 import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException;
+import org.codehaus.groovy.grails.web.util.StreamByteBuffer;
+import org.codehaus.groovy.grails.web.util.StreamCharBuffer;
 
 import java.io.*;
 import java.util.*;
@@ -89,6 +93,7 @@ public class GroovyPageParser implements Tokens {
     };
     private static final String CONFIG_PROPERTY_DEFAULT_CODEC = "grails.views.default.codec";
     private static final String CONFIG_PROPERTY_GSP_ENCODING = "grails.views.gsp.encoding";
+    private static final String CONFIG_PROPERTY_GSP_KEEPGENERATED_DIR = "grails.views.gsp.keepgenerateddir";
 
     private String codecClassName;
     private String codecName;
@@ -101,6 +106,8 @@ public class GroovyPageParser implements Tokens {
     private String gspEncoding;
     public static final String GROOVY_SOURCE_CHAR_ENCODING = "UTF-8";
     private Map jspTags = new HashMap();
+    
+    private File keepGeneratedDirectory;
 
     public String getContentType() {
         return this.contentType;
@@ -148,7 +155,17 @@ public class GroovyPageParser implements Tokens {
         makeName(name);
         Object o = config.get(CONFIG_PROPERTY_DEFAULT_CODEC);
         lookupCodec(o);
-
+        
+        Object keepDirObj = config.get(CONFIG_PROPERTY_GSP_KEEPGENERATED_DIR);
+        if(keepDirObj instanceof File) {
+        	keepGeneratedDirectory=((File)keepDirObj);
+        } else if (keepDirObj != null) {
+        	keepGeneratedDirectory=new File(String.valueOf(keepDirObj));
+        }
+        if(keepGeneratedDirectory != null && !keepGeneratedDirectory.isDirectory()) {
+        	LOG.warn("The directory specified with " + CONFIG_PROPERTY_GSP_KEEPGENERATED_DIR + " config parameter doesn't exist or isn't a readable directory. Absolute path: '" + keepGeneratedDirectory.getAbsolutePath() + "' Keepgenerated will be disabled.");
+        	keepGeneratedDirectory=null;
+        }
     } // Parse()
 
     private void lookupCodec(Object o) {
@@ -168,30 +185,54 @@ public class GroovyPageParser implements Tokens {
     public int[] getLineNumberMatrix() {
         return out.getLineNumbers();
     }
+    
     public InputStream parse() {
+    	StreamCharBuffer streamBuffer=new StreamCharBuffer(1024);
+    	StreamByteBuffer byteOutputBuffer=new StreamByteBuffer(1024);
+    	
+    	try {
+    		streamBuffer.connectTo(new OutputStreamWriter(byteOutputBuffer.getOutputStream(), GROOVY_SOURCE_CHAR_ENCODING), true);
+	    } catch (UnsupportedEncodingException e) {
+	        throw new RuntimeException("Grails cannot run unless your environment supports UTF-8!");
+	    }
+	    
+	    File keepGeneratedFile = null;
+    	Writer keepGeneratedWriter = null;
+    	if(keepGeneratedDirectory != null) {
+    		keepGeneratedFile = new File(keepGeneratedDirectory, className);
+	    	try {
+	    		keepGeneratedWriter = new OutputStreamWriter(new FileOutputStream(keepGeneratedFile), GROOVY_SOURCE_CHAR_ENCODING);
+	    	} catch (IOException e) {
+	    		LOG.warn("Cannot open keepgenerated file for writing. File's absolute path is '" + keepGeneratedFile.getAbsolutePath() + "'");
+	    		keepGeneratedFile=null;
+	    	}
+	    	streamBuffer.connectTo(keepGeneratedWriter, true);
+	    }
 
-        StringWriter sw = new StringWriter();
-        out = new GSPWriter(sw,this);
-        page();
-        finalPass = true;
-        scan.reset();
-        previousContentWasNonWhitespace = false;
-        currentlyBufferingWhitespace = false;
-        page();
-
-        // This gets bytes in system's default encoding
-        InputStream in = null;
-        try {
-            in = new ByteArrayInputStream(sw.toString().getBytes(GROOVY_SOURCE_CHAR_ENCODING));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("Grails cannot run unless your environment supports UTF-8!");
-        }
-        //System.out.println("Compiled GSP into Groovy code: " + sw.toString());
-        if(LOG.isDebugEnabled()) {
-            LOG.debug("Compiled GSP into Groovy code: " + sw.toString());
-        }
-        scan = null;
-        return in;
+    	try {
+		    out = new GSPWriter(streamBuffer.getWriter(),this);
+	        page();
+	        finalPass = true;
+	        scan.reset();
+	        previousContentWasNonWhitespace = false;
+	        currentlyBufferingWhitespace = false;
+	        page();
+	        
+	        out.flush();	
+	        
+	        if(LOG.isDebugEnabled()) {
+	        	if(keepGeneratedFile != null) {
+	        		LOG.debug("Compiled GSP into Groovy code. Source is in " + keepGeneratedFile);
+	        	}  else {
+	        		LOG.debug("Configure " + CONFIG_PROPERTY_GSP_KEEPGENERATED_DIR + " property to view generated source.");
+	        	}
+	        }
+	        scan = null;
+	        InputStream in = byteOutputBuffer.getInputStream();
+	        return in;
+    	} finally {
+    		IOUtils.closeQuietly(keepGeneratedWriter);
+    	}
     } 
 
 
@@ -754,20 +795,8 @@ public class GroovyPageParser implements Tokens {
     }
 
     private String readStream(InputStream in) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        try {
-            byte[] buf = new byte[8192];
-            for (;;) {
-                int read = in.read(buf);
-                if (read <= 0) break;
-                out.write(buf, 0, read);
-            }
-            return out.toString( gspEncoding);
-        } finally {
-            out.close();
-            in.close();
-        }
-    } // readStream()
+    	return IOUtils.toString(in, gspEncoding);
+    }
 
     private void script(boolean gsp) {
         if (!finalPass) return;

grails/src/web/org/codehaus/groovy/grails/web/util/StreamByteBuffer.java
@@ -1,0 +1,311 @@
+package org.codehaus.groovy.grails.web.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ *
+ * StreamByteBuffer is a in-memory buffer that provides OutputStream and
+ * InputStream interfaces
+ *
+ * This is more efficient than using ByteArrayOutputStream/ByteArrayInputStream
+ *
+ * This is not thread-safe, it is intended to be used by a single Thread.
+ *
+ *
+ * @author Lari Hotari, Sagire Software Oy
+ *
+ */
+public class StreamByteBuffer {
+	private static final int DEFAULT_CHUNK_SIZE = 8192;
+
+	private LinkedList<StreamByteBufferChunk> chunks = new LinkedList<StreamByteBufferChunk>();
+	private StreamByteBufferChunk currentWriteChunk;
+	private StreamByteBufferChunk currentReadChunk = null;
+	private int chunkSize;
+	private StreamByteBufferOutputStream output;
+	private StreamByteBufferInputStream input;
+	private int totalBytesUnreadInList = 0;
+
+	public StreamByteBuffer() {
+		this(DEFAULT_CHUNK_SIZE);
+	}
+
+	public StreamByteBuffer(int chunkSize) {
+		this.chunkSize = chunkSize;
+		currentWriteChunk = new StreamByteBufferChunk(chunkSize);
+		output = new StreamByteBufferOutputStream();
+		input = new StreamByteBufferInputStream();
+	}
+
+	public OutputStream getOutputStream() {
+		return output;
+	}
+
+	public InputStream getInputStream() {
+		return input;
+	}
+
+	public void writeTo(OutputStream target) throws IOException {
+		while (prepareRead() != -1) {
+			currentReadChunk.writeTo(target);
+		}
+	}
+
+	public byte[] readAsByteArray() {
+		byte[] buf = new byte[totalBytesUnread()];
+		input.readImpl(buf, 0, buf.length);
+		return buf;
+	}
+
+	public String readAsString(String encoding) throws CharacterCodingException {
+		Charset charset = Charset.forName(encoding);
+		return readAsString(charset);
+	}
+
+	public String readAsString(Charset charset) throws CharacterCodingException {
+		int unreadSize = totalBytesUnread();
+		if (unreadSize > 0) {
+			CharsetDecoder decoder = charset.newDecoder().onMalformedInput(
+					CodingErrorAction.REPLACE).onUnmappableCharacter(
+					CodingErrorAction.REPLACE);
+			CharBuffer charbuffer = CharBuffer.allocate(unreadSize);
+			ByteBuffer buf = null;
+			while (prepareRead() != -1) {
+				buf = currentReadChunk.readToNioBuffer();
+				boolean endOfInput = (prepareRead() == -1);
+				CoderResult result = decoder
+						.decode(buf, charbuffer, endOfInput);
+				if (endOfInput) {
+					if(!result.isUnderflow()) {
+						result.throwException();
+					}
+				}
+			}
+			CoderResult result = decoder.flush(charbuffer);
+			if(buf.hasRemaining()) {
+				throw new IllegalStateException("There's a bug here, buffer wasn't read fully.");
+			}
+			if (!result.isUnderflow())
+				result.throwException();
+			charbuffer.flip();
+			String str;
+			if(charbuffer.hasArray()) {
+				int len=charbuffer.remaining();
+				char[] ch=charbuffer.array();
+				if(len != ch.length) {
+					ch=Arrays.copyOfRange(ch, 0, len);
+				}
+				str=StringCharArrayAccessor.createString(ch);
+			} else {
+				str=charbuffer.toString();
+			}
+			return str;
+		}
+		return null;
+	}
+
+	public int totalBytesUnread() {
+		int total = totalBytesUnreadInList;
+		if (currentReadChunk != null) {
+			total += currentReadChunk.bytesUnread();
+		}
+		if (currentWriteChunk != currentReadChunk && currentWriteChunk != null) {
+			total += currentWriteChunk.bytesUnread();
+		}
+		return total;
+	}
+
+	protected int allocateSpace() {
+		int spaceLeft = currentWriteChunk.spaceLeft();
+		if (spaceLeft == 0) {
+			chunks.add(currentWriteChunk);
+			totalBytesUnreadInList += currentWriteChunk.bytesUnread();
+			currentWriteChunk = new StreamByteBufferChunk(chunkSize);
+			spaceLeft = currentWriteChunk.spaceLeft();
+		}
+		return spaceLeft;
+	}
+
+	protected int prepareRead() {
+		int bytesUnread = (currentReadChunk != null) ? currentReadChunk
+				.bytesUnread() : 0;
+		if (bytesUnread == 0) {
+			if (!chunks.isEmpty()) {
+				currentReadChunk = chunks.removeFirst();
+				bytesUnread = currentReadChunk.bytesUnread();
+				totalBytesUnreadInList -= bytesUnread;
+			} else if (currentReadChunk != currentWriteChunk) {
+				currentReadChunk = currentWriteChunk;
+				bytesUnread = currentReadChunk.bytesUnread();
+			} else {
+				bytesUnread = -1;
+			}
+		}
+		return bytesUnread;
+	}
+
+	class StreamByteBufferChunk {
+		private int pointer = 0;
+		private byte[] buffer;
+		private int size;
+		private int used = 0;
+
+		public StreamByteBufferChunk(int size) {
+			this.size = size;
+			this.buffer = new byte[size];
+		}
+
+		public ByteBuffer readToNioBuffer() {
+			if (pointer < used) {
+				ByteBuffer result;
+				if (pointer > 0 || used < size) {
+					result=ByteBuffer.wrap(buffer, pointer, used - pointer);
+				} else {
+					result=ByteBuffer.wrap(buffer);
+				}
+				pointer = used;
+				return result;
+			} else {
+				return null;
+			}
+		}
+
+		public boolean write(byte b) {
+			if (used < size) {
+				buffer[used++] = b;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public void write(byte[] b, int off, int len) {
+			System.arraycopy(b, off, buffer, used, len);
+			used = used + len;
+		}
+
+		public void read(byte[] b, int off, int len) {
+			System.arraycopy(buffer, pointer, b, off, len);
+			pointer = pointer + len;
+		}
+
+		public void writeTo(OutputStream target) throws IOException {
+			if (pointer < used) {
+				target.write(buffer, pointer, used - pointer);
+				pointer = used;
+			}
+		}
+
+		public void reset() {
+			this.pointer = 0;
+		}
+
+		public int bytesUnread() {
+			return used - pointer;
+		}
+
+		public int read() {
+			if (pointer < used) {
+				return buffer[pointer++] & 0xff;
+			} else {
+				return -1;
+			}
+		}
+
+		public int spaceLeft() {
+			return size - used;
+		}
+	}
+
+	class StreamByteBufferOutputStream extends OutputStream {
+		private boolean closed = false;
+
+		public void write(byte[] b, int off, int len) throws IOException {
+			if (b == null) {
+				throw new NullPointerException();
+			} else if ((off < 0) || (off > b.length) || (len < 0)
+					|| ((off + len) > b.length) || ((off + len) < 0)) {
+				throw new IndexOutOfBoundsException();
+			} else if (len == 0) {
+				return;
+			}
+
+			int bytesLeft = len;
+			int currentOffset = off;
+			while (bytesLeft > 0) {
+				int spaceLeft = allocateSpace();
+				int writeBytes = Math.min(spaceLeft, bytesLeft);
+				currentWriteChunk.write(b, currentOffset, writeBytes);
+				bytesLeft -= writeBytes;
+				currentOffset += writeBytes;
+			}
+		}
+
+		public void close() throws IOException {
+			this.closed = true;
+		}
+
+		public boolean isClosed() {
+			return this.closed;
+		}
+
+		public void write(int b) throws IOException {
+			allocateSpace();
+			currentWriteChunk.write((byte) b);
+		}
+	}
+
+	class StreamByteBufferInputStream extends InputStream {
+		public int read() throws IOException {
+			prepareRead();
+			return currentReadChunk.read();
+		}
+
+		public int read(byte[] b, int off, int len) throws IOException {
+			return readImpl(b, off, len);
+		}
+
+		int readImpl(byte[] b, int off, int len) {
+			if (b == null) {
+				throw new NullPointerException();
+			} else if ((off < 0) || (off > b.length) || (len < 0)
+					|| ((off + len) > b.length) || ((off + len) < 0)) {
+				throw new IndexOutOfBoundsException();
+			} else if (len == 0) {
+				return 0;
+			}
+			int bytesLeft = len;
+			int currentOffset = off;
+			int bytesUnread = prepareRead();
+			int totalBytesRead = 0;
+			while (bytesLeft > 0 && bytesUnread != -1) {
+				int readBytes = Math.min(bytesUnread, bytesLeft);
+				currentReadChunk.read(b, currentOffset, readBytes);
+				bytesLeft -= readBytes;
+				currentOffset += readBytes;
+				totalBytesRead += readBytes;
+				bytesUnread = prepareRead();
+			}
+			if (totalBytesRead > 0) {
+				return totalBytesRead;
+			} else {
+				return -1;
+			}
+		}
+
+		public int available() throws IOException {
+			return totalBytesUnread();
+		}
+	}
+}

grails/test/web/org/codehaus/groovy/grails/web/util/StreamByteBufferTest.java
@@ -1,0 +1,208 @@
+package org.codehaus.groovy.grails.web.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for StreamByteBuffer
+ *
+ *
+ * @author Lari Hotari, Sagire Software Oy
+ *
+ */
+public class StreamByteBufferTest extends TestCase {
+	private static final int TESTROUNDS = 10000;
+	static byte[] testbuffer = new byte[256 * TESTROUNDS];
+
+	@Override
+	protected void setUp() throws Exception {
+		if (testbuffer == null) {
+			for (int i = 0; i < TESTROUNDS; i++) {
+				for (int j = 0; j < 256; j++) {
+					testbuffer[i * 256 + j] = (byte) (j & 0xff);
+				}
+			}
+		}
+	}
+
+	public void testToByteArray() throws IOException {
+		StreamByteBuffer byteBuffer = createTestInstance();
+		byte[] result = byteBuffer.readAsByteArray();
+		assertTrue(Arrays.equals(testbuffer, result));
+	}
+
+	public void testToString() throws IOException {
+		StreamByteBuffer byteBuffer = new StreamByteBuffer();
+		PrintWriter pw=new PrintWriter(new OutputStreamWriter(byteBuffer.getOutputStream(),"UTF-8"));
+		pw.print("Hello öäåÖÄÅ");
+		pw.close();
+		assertEquals("Hello öäåÖÄÅ", byteBuffer.readAsString("UTF-8"));
+	}
+
+	public void testToInputStream() throws IOException {
+		StreamByteBuffer byteBuffer = createTestInstance();
+		InputStream input = byteBuffer.getInputStream();
+		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream(byteBuffer
+				.totalBytesUnread());
+		copy(input, bytesOut, 2048);
+		byte[] result = bytesOut.toByteArray();
+		assertTrue(Arrays.equals(testbuffer, result));
+	}
+
+	public void testStreamByteBuffer() throws Exception {
+		StreamByteBuffer streamBuf=new StreamByteBuffer(32000);
+		OutputStream output=streamBuf.getOutputStream();
+		output.write(1);
+		output.write(2);
+		output.write(3);
+		output.write(255);
+		output.close();
+		InputStream input=streamBuf.getInputStream();
+		assertEquals(1, input.read());
+		assertEquals(2, input.read());
+		assertEquals(3, input.read());
+		assertEquals(255, input.read());
+		assertEquals(-1, input.read());
+		input.close();
+	}
+
+	public void testStreamByteBuffer2() throws Exception {
+		StreamByteBuffer streamBuf=new StreamByteBuffer(32000);
+		OutputStream output=streamBuf.getOutputStream();
+		byte[] bytes=new byte[]{(byte)1,(byte)2,(byte)3};
+		output.write(bytes);
+		output.close();
+		InputStream input=streamBuf.getInputStream();
+		assertEquals(1, input.read());
+		assertEquals(2, input.read());
+		assertEquals(3, input.read());
+		assertEquals(-1, input.read());
+		input.close();
+	}
+
+	public void testStreamByteBuffer3() throws Exception {
+		bufferTest(10000,10000);
+		bufferTest(1,10000);
+		bufferTest(2,10000);
+		bufferTest(10000,2);
+		bufferTest(10000,1);
+
+		bufferTest2(10000,10000);
+		bufferTest2(1,10000);
+		bufferTest2(2,10000);
+		bufferTest2(10000,2);
+		bufferTest2(10000,1);
+	}
+
+	private void bufferTest(int streamByteBufferSize, int testBufferSize) throws IOException {
+		StreamByteBuffer streamBuf=new StreamByteBuffer(streamByteBufferSize);
+		OutputStream output=streamBuf.getOutputStream();
+		for(int i=0;i < testBufferSize;i++) {
+			output.write(i % (Byte.MAX_VALUE*2));
+		}
+		output.close();
+		byte[] buffer=new byte[testBufferSize];
+		InputStream input=streamBuf.getInputStream();
+		assertEquals(testBufferSize, input.available());
+		int readBytes=input.read(buffer);
+		assertEquals(readBytes, testBufferSize);
+		for(int i=0;i < buffer.length;i++) {
+			assertEquals((byte)(i % (Byte.MAX_VALUE*2)), buffer[i]);
+		}
+		assertEquals(-1, input.read());
+		assertEquals(-1, input.read());
+		assertEquals(-1, input.read());
+		assertEquals(-1, input.read());
+		input.close();
+	}
+
+	private void bufferTest2(int streamByteBufferSize, int testBufferSize) throws IOException {
+		StreamByteBuffer streamBuf=new StreamByteBuffer(streamByteBufferSize);
+		OutputStream output=streamBuf.getOutputStream();
+		for(int i=0;i < testBufferSize;i++) {
+			output.write(i % (Byte.MAX_VALUE*2));
+		}
+		output.close();
+		InputStream input=streamBuf.getInputStream();
+		assertEquals(testBufferSize, input.available());
+		for(int i=0;i < testBufferSize;i++) {
+			assertEquals((i % (Byte.MAX_VALUE*2)), input.read());
+		}
+		assertEquals(-1, input.read());
+		assertEquals(-1, input.read());
+		assertEquals(-1, input.read());
+		assertEquals(-1, input.read());
+		input.close();
+	}
+
+	public void testWriteTo() throws IOException {
+		StreamByteBuffer byteBuffer = createTestInstance();
+		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream(byteBuffer
+				.totalBytesUnread());
+		byteBuffer.writeTo(bytesOut);
+		byte[] result = bytesOut.toByteArray();
+		assertTrue(Arrays.equals(testbuffer, result));
+	}
+
+	public void testToInputStreamOneByOne() throws IOException {
+		StreamByteBuffer byteBuffer = createTestInstance();
+		InputStream input = byteBuffer.getInputStream();
+		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream(byteBuffer
+				.totalBytesUnread());
+		copyOneByOne(input, bytesOut);
+		byte[] result = bytesOut.toByteArray();
+		assertTrue(Arrays.equals(testbuffer, result));
+	}
+
+	private int copy(InputStream input, OutputStream output, int bufSize)
+			throws IOException {
+		byte[] buffer = new byte[bufSize];
+		int count = 0;
+		int n = 0;
+		while (-1 != (n = input.read(buffer))) {
+			output.write(buffer, 0, n);
+			count += n;
+		}
+		return count;
+	}
+
+	private int copyOneByOne(InputStream input, OutputStream output)
+			throws IOException {
+		int count = 0;
+		int b;
+		while (-1 != (b = input.read())) {
+			output.write(b);
+			count++;
+		}
+		return count;
+	}
+
+	private StreamByteBuffer createTestInstance() throws IOException {
+		StreamByteBuffer byteBuffer = new StreamByteBuffer();
+		OutputStream output = byteBuffer.getOutputStream();
+		copyAllFromTestBuffer(output, 27);
+		return byteBuffer;
+	}
+
+	private void copyAllFromTestBuffer(OutputStream output, int partsize)
+			throws IOException {
+		int position = 0;
+		int bytesLeft = testbuffer.length;
+		while (bytesLeft > 0) {
+			output.write(testbuffer, position, partsize);
+			position += partsize;
+			bytesLeft -= partsize;
+			if (bytesLeft < partsize) {
+				partsize = bytesLeft;
+			}
+		}
+	}
+
+}

osscontributor/grailsnolib

commit 1af7d92643b0c0b339b0087c645a593b2a990f82

Fix for failing test org.codehaus.groovy.grails.web.pages/GroovyPagesTemplateEngineTests/testShowSourceParameter. Broke in commit http://github.com/grails/grails/commit/4d7965c8c9f0cc70c74398a829fccd4fdfe81852 related to GRAILS-4422 . StreamByteBuffer's InputStream interface supports reset() method now.

src/web/org/codehaus/groovy/grails/web/pages/GroovyPageParser.java
@@ -188,7 +188,7 @@ public class GroovyPageParser implements Tokens {
     
     public InputStream parse() {
     	StreamCharBuffer streamBuffer=new StreamCharBuffer(1024);
-    	StreamByteBuffer byteOutputBuffer=new StreamByteBuffer(1024);
+    	StreamByteBuffer byteOutputBuffer=new StreamByteBuffer(1024, StreamByteBuffer.ReadMode.RETAIN_AFTER_READING);
     	
     	try {
     		streamBuffer.connectTo(new OutputStreamWriter(byteOutputBuffer.getOutputStream(), GROOVY_SOURCE_CHAR_ENCODING), true);

src/web/org/codehaus/groovy/grails/web/util/StreamByteBuffer.java
@@ -8,6 +8,7 @@ import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.*;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -33,13 +34,26 @@ public class StreamByteBuffer {
 	private StreamByteBufferOutputStream output;
 	private StreamByteBufferInputStream input;
 	private int totalBytesUnreadInList = 0;
+	private int totalBytesUnreadInIterator = 0;
+	private ReadMode readMode;
+	private Iterator<StreamByteBufferChunk> readIterator;
+	
+	public enum ReadMode {
+		REMOVE_AFTER_READING,
+		RETAIN_AFTER_READING
+	}
 
 	public StreamByteBuffer() {
 		this(DEFAULT_CHUNK_SIZE);
 	}
-
+	
 	public StreamByteBuffer(int chunkSize) {
+		this(chunkSize, ReadMode.REMOVE_AFTER_READING);
+	}
+
+	public StreamByteBuffer(int chunkSize, ReadMode readMode) {
 		this.chunkSize = chunkSize;
+		this.readMode = readMode;
 		currentWriteChunk = new StreamByteBufferChunk(chunkSize);
 		output = new StreamByteBufferOutputStream();
 		input = new StreamByteBufferInputStream();
@@ -113,12 +127,22 @@ public class StreamByteBuffer {
 	}
 
 	public int totalBytesUnread() {
-		int total = totalBytesUnreadInList;
+		int total=0;
+		if(readMode == ReadMode.REMOVE_AFTER_READING) {
+			total = totalBytesUnreadInList;
+		} else if (readMode == ReadMode.RETAIN_AFTER_READING) {
+			prepareRetainAfterReading();
+			total = totalBytesUnreadInIterator;
+		}
 		if (currentReadChunk != null) {
 			total += currentReadChunk.bytesUnread();
 		}
 		if (currentWriteChunk != currentReadChunk && currentWriteChunk != null) {
-			total += currentWriteChunk.bytesUnread();
+			if(readMode == ReadMode.REMOVE_AFTER_READING) {
+				total += currentWriteChunk.bytesUnread();
+			} else if (readMode == ReadMode.RETAIN_AFTER_READING) {
+				total += currentWriteChunk.bytesUsed();
+			}
 		}
 		return total;
 	}
@@ -135,13 +159,19 @@ public class StreamByteBuffer {
 	}
 
 	protected int prepareRead() {
+		prepareRetainAfterReading();
 		int bytesUnread = (currentReadChunk != null) ? currentReadChunk
 				.bytesUnread() : 0;
 		if (bytesUnread == 0) {
-			if (!chunks.isEmpty()) {
+			if (readMode==ReadMode.REMOVE_AFTER_READING && !chunks.isEmpty()) {
 				currentReadChunk = chunks.removeFirst();
 				bytesUnread = currentReadChunk.bytesUnread();
 				totalBytesUnreadInList -= bytesUnread;
+			} else if (readMode==ReadMode.RETAIN_AFTER_READING && readIterator.hasNext()) {
+				currentReadChunk = readIterator.next();
+				currentReadChunk.reset();
+				bytesUnread = currentReadChunk.bytesUnread();
+				totalBytesUnreadInIterator -= bytesUnread;
 			} else if (currentReadChunk != currentWriteChunk) {
 				currentReadChunk = currentWriteChunk;
 				bytesUnread = currentReadChunk.bytesUnread();
@@ -150,6 +180,36 @@ public class StreamByteBuffer {
 			}
 		}
 		return bytesUnread;
+	}
+	
+	public void reset() {
+		if(readMode==ReadMode.RETAIN_AFTER_READING) {
+			readIterator=null;
+			prepareRetainAfterReading();
+			if(currentWriteChunk != null) {
+				currentWriteChunk.reset();
+			}
+		}
+	}
+
+	private void prepareRetainAfterReading() {
+		if(readMode==ReadMode.RETAIN_AFTER_READING && readIterator==null) {
+			readIterator=chunks.iterator();
+			totalBytesUnreadInIterator = totalBytesUnreadInList;
+			currentReadChunk=null;
+		}
+	}
+
+	public ReadMode getReadMode() {
+		return readMode;
+	}
+
+	public void setReadMode(ReadMode readMode) {
+		this.readMode = readMode;
+	}
+	
+	public void retainAfterReadingMode() {
+		setReadMode(ReadMode.RETAIN_AFTER_READING);
 	}
 
 	class StreamByteBufferChunk {
@@ -207,6 +267,10 @@ public class StreamByteBuffer {
 		public void reset() {
 			this.pointer = 0;
 		}
+		
+		public int bytesUsed() {
+			return used;
+		}
 
 		public int bytesUnread() {
 			return used - pointer;
@@ -261,6 +325,10 @@ public class StreamByteBuffer {
 			allocateSpace();
 			currentWriteChunk.write((byte) b);
 		}
+		
+		public StreamByteBuffer getBuffer() {
+			return StreamByteBuffer.this;
+		}
 	}
 
 	class StreamByteBufferInputStream extends InputStream {
@@ -301,8 +369,22 @@ public class StreamByteBuffer {
 			}
 		}
 
+		@Override
+		public synchronized void reset() throws IOException {
+			if(readMode==ReadMode.RETAIN_AFTER_READING) {
+				StreamByteBuffer.this.reset();
+			} else {
+				// reset isn't supported in ReadMode.REMOVE_AFTER_READING
+				super.reset();
+			}
+		}
+
 		public int available() throws IOException {
 			return totalBytesUnread();
+		}
+
+		public StreamByteBuffer getBuffer() {
+			return StreamByteBuffer.this;
 		}
 	}
 }

test/web/org/codehaus/groovy/grails/web/util/StreamByteBufferTest.java
@@ -46,6 +46,17 @@ public class StreamByteBufferTest extends TestCase {
 		assertEquals("Hello öäåÖÄÅ", byteBuffer.readAsString("UTF-8"));
 	}
 
+	public void testToStringRetain() throws IOException {
+		StreamByteBuffer byteBuffer = new StreamByteBuffer(1024, StreamByteBuffer.ReadMode.RETAIN_AFTER_READING);
+		PrintWriter pw=new PrintWriter(new OutputStreamWriter(byteBuffer.getOutputStream(),"UTF-8"));
+		pw.print("Hello öäåÖÄÅ");
+		pw.close();
+		assertEquals("Hello öäåÖÄÅ", byteBuffer.readAsString("UTF-8"));
+		byteBuffer.reset();
+		// call a second time to test if the RETAIN_AFTER_READING mode works as expected
+		assertEquals("Hello öäåÖÄÅ", byteBuffer.readAsString("UTF-8"));
+	}
+	
 	public void testToInputStream() throws IOException {
 		StreamByteBuffer byteBuffer = createTestInstance();
 		InputStream input = byteBuffer.getInputStream();